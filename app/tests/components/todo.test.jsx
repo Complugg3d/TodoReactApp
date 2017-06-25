3f57b0afd3248bc987446bf24481a181bc9cb0c3
Erick Garcia <erick.m.garcia.vega@gmail.com>
2017-06-25 15:37:00 -0500

    added test for list component

---
+++ app/tests/components/todo.test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+const ReactDOM = require('react-dom');
+import expect from 'expect';
+import $ from 'jquery';
+import TestUtils from 'react-addons-test-utils';
+import Todo from 'todo';
+
+describe('Todo', () => {
+  it('should exist', () => {
+    expect(Todo).toExist();
+  });
+
+});
