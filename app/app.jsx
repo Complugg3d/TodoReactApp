63ed10d64e6851d5f391a63efe176383ec810bde
Erick Garcia <erick.m.garcia.vega@gmail.com>
2017-09-17 18:05:38 -0500

    add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,17 @@ const ReactDOM = require('react-dom');
 import { Route, Router, IndexRoute, hashHistory } from 'react-router';
 import TodoApp from 'todo-app';
 
+const actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+  console.log('New state', store.getState());
+});
+
+store.dispatch(actions.addTodo('Clean the yard'));
+store.dispatch(actions.setSearchText('Yard'));
+store.dispatch(actions.toggleShowCompleted());
+
 //load foundation
 //require('style!css!foundation-sites/dist/css/foundation.min.css');
 $(document).foundation();
