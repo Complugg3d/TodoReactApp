102046d4905e65b7b74462204ca424b2f56230fc
erick.mgv <erick.mgv@live.com>
2017-10-31 11:15:15 -0400

    adding check login reducer

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,13 +1,14 @@
 import * as redux from 'redux';
 import thunk from 'redux-thunk';
 
-import { searchTextReducer, showCompletedReducer, todosReducer } from 'reducers';
+import { searchTextReducer, showCompletedReducer, todosReducer, checkLoginReducer } from 'reducers';
 
 export var configure = (initialState = {}) => {
   var reducer = redux.combineReducers({
     searchText: searchTextReducer,
     showCompleted: showCompletedReducer,
-    todos: todosReducer
+    todos: todosReducer,
+    checkLogin: checkLoginReducer
   });
 
   var store = redux.createStore(reducer, initialState, redux.compose(
